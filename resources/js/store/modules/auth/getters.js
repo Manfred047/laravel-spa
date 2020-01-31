0581f4c377a419d6247dc0813219876ddb036c1f
Manfred <manfred@manfred047.com>
2020-01-31 00:46:12 -0600

    Update getters.js

--- resources/js/store/modules/auth/getters.js
+++ resources/js/store/modules/auth/getters.js
@@ -1,35 +1,39 @@
 import _ from 'lodash'
-import { master } from '../../helpers/master'
-import { LocalStorage } from 'quasar'
+import { master } from '../../../helpers/master'
+
 export default {
   isAuth (state) {
     let auth = _.get(state, ['is_auth'], false)
     if (auth) {
       return auth
     }
-    if (LocalStorage.has(master.getAuthTokenName())) {
+    let authToken = localStorage.getItem(master.getAuthTokenName())
+    if (!_.isEmpty(authToken)) {
       return true
     }
     return _.get(state, ['is_auth'], false)
   },
   username (state) {
     let username = _.get(state, ['user_data', 'username'])
-    if (!username && LocalStorage.has(master.getStorageUserDataName())) {
-      return _.get(LocalStorage.getItem(master.getStorageUserDataName()), ['username'], '')
+    let userData = localStorage.getItem(master.getStorageUserDataName())
+    if (!username && !_.isEmpty(userData)) {
+      return _.get(userData, ['username'], '')
     }
     return username
   },
   email (state) {
     let email = _.get(state, ['user_data', 'email'])
-    if (!email && LocalStorage.has(master.getStorageUserDataName())) {
-      return _.get(LocalStorage.getItem(master.getStorageUserDataName()), ['email'], '')
+    let userData = localStorage.getItem(master.getStorageUserDataName())
+    if (!email && !_.isEmpty(userData)) {
+      return _.get(userData, ['email'], '')
     }
     return email
   },
   userData (state) {
     let data = _.get(state, ['user_data', 'email'])
-    if (!data && LocalStorage.has(master.getStorageUserDataName())) {
-      return LocalStorage.getItem(master.getStorageUserDataName()) || {}
+    let userData = localStorage.getItem(master.getStorageUserDataName())
+    if (!data && !_.isEmpty(userData)) {
+      return userData || {}
     }
     return data
   }
