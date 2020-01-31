9404b9cf1cb8c670ab2ec9b523543eba2cce0699
Manfred <manfred@manfred047.com>
2020-01-31 00:46:01 -0600

    Update PublicLayout.vue

--- resources/js/layouts/public/PublicLayout.vue
+++ resources/js/layouts/public/PublicLayout.vue
@@ -1,26 +1,28 @@
 <template>
-  <v-navigation-drawer app>
-    <!-- -->
-  </v-navigation-drawer>
+  <div>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
 
-  <v-app-bar app>
-    <!-- -->
-  </v-app-bar>
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
 
-  <!-- Sizes your content based upon application components -->
-  <v-content>
+    <!-- Sizes your content based upon application components -->
+    <v-content>
 
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
 
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-  </v-content>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-content>
 
-  <v-footer app>
-    <!-- -->
-  </v-footer>
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </div>
 </template>
 
 <script>
