ad594c0ab81e2866909b32e83e77dfe05e50532b
Manfred <manfred@manfred047.com>
2020-01-31 00:46:23 -0600

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,13 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link type="text/css" href="{{ mix('css/app.css') }}" rel="stylesheet">
+
   <title>Laravel web SPA</title>
 </head>
   <body>
     <div id="app"></div>
+    <script type="text/javascript" charset="UTF-8" src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
