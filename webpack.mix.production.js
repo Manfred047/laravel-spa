a6f47792839c7839dde8c942881f6443786fc2c6
Manfred <manfred@manfred047.com>
2020-01-31 19:09:20 -0600

    Update webpack.mix.production.js

--- webpack.mix.production.js
+++ webpack.mix.production.js
@@ -1,6 +1,17 @@
 const mix = require('laravel-mix')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 mix.setPublicPath('public/production')
+
+mix.webpackConfig({
+  output: {
+    publicPath: 'production/',
+    chunkFilename: 'chunks/[name].[chunkhash].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+})
 
 mix.js('resources/js/app.js', 'js')
 mix.sass('resources/sass/app.scss', 'css')
