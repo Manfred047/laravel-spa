7f674ed72376120d8c12fbe72a56f3c7a32ffe18
Manfred <manfred@manfred047.com>
2020-01-31 19:09:16 -0600

    Update webpack.mix.development.js

--- webpack.mix.development.js
+++ webpack.mix.development.js
@@ -1,6 +1,19 @@
 const mix = require('laravel-mix')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 mix.setPublicPath('public/sandbox')
+
+mix.webpackConfig({
+  output: {
+    publicPath: 'sandbox/',
+    chunkFilename: 'chunks/[name].[chunkhash].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['!.gitkeep']
+    })
+  ]
+})
 
 mix.js('resources/js/app.js', 'js')
 mix.sass('resources/sass/app.scss', 'css')
